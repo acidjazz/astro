14de327d18a516802c3d928352718c4b02c4b436
kevin olson <acidjazz@gmail.com>
2015-08-06 05:19:46 -0700

    real image loading

--- cof/project.coffee
+++ cof/project.coffee
@@ -3,5 +3,40 @@ Project =
 
   i: ->
 
-    console.log 'Project.i()'
+    
+
+    srcs = Project.srcs 'versus'
+    console.log srcs
+    Project.preload srcs,
+      (progress) ->
+        console.log 'progress', progress
+      , (complete) ->
+        console.log 'complete', complete
+        _.on '.project_versus'
+
+  preload: (srces, progress, complete) ->
+
+    images = []
+    loaded = 0
+    total = srces.length
+
+    for src, i in srces
+      images[i] = new Image()
+      images[i].src = src
+      images[i].onload = ->
+        loaded++
+        perc = Math.round(loaded/total*10)/10
+        if loaded is total then complete(true) else progress(perc)
+
+    
+
+  srcs: (project) ->
+    cover = $(".project_#{project} > .cover").attr 'style'
+    url = cover.match(/url\("(.*)"\)/)
+    srcs = [url[1]]
+    $(".project_#{project} img").each (i, v) ->
+      console.log $(v).attr 'src'
+      srcs.push $(v).attr 'src'
+
+    return srcs
 
--- pub/jst/project.js
+++ pub/jst/project.js
@@ -2,6 +2,48 @@ var Project;
 
 Project = {
   i: function() {
-    return console.log('Project.i()');
+    var srcs;
+    srcs = Project.srcs('versus');
+    console.log(srcs);
+    return Project.preload(srcs, function(progress) {
+      return console.log('progress', progress);
+    }, function(complete) {
+      console.log('complete', complete);
+      return _.on('.project_versus');
+    });
+  },
+  preload: function(srces, progress, complete) {
+    var i, images, j, len, loaded, results, src, total;
+    images = [];
+    loaded = 0;
+    total = srces.length;
+    results = [];
+    for (i = j = 0, len = srces.length; j < len; i = ++j) {
+      src = srces[i];
+      images[i] = new Image();
+      images[i].src = src;
+      results.push(images[i].onload = function() {
+        var perc;
+        loaded++;
+        perc = Math.round(loaded / total * 10) / 10;
+        if (loaded === total) {
+          return complete(true);
+        } else {
+          return progress(perc);
+        }
+      });
+    }
+    return results;
+  },
+  srcs: function(project) {
+    var cover, srcs, url;
+    cover = $(".project_" + project + " > .cover").attr('style');
+    url = cover.match(/url\("(.*)"\)/);
+    srcs = [url[1]];
+    $(".project_" + project + " img").each(function(i, v) {
+      console.log($(v).attr('src'));
+      return srcs.push($(v).attr('src'));
+    });
+    return srcs;
   }
 };

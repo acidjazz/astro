f522cb938734fb3817a327bad8b77a32464b40dc
kevin olson <acidjazz@gmail.com>
2015-08-19 16:42:18 -0700

    latest

--- cof/about.coffee
+++ cof/about.coffee
@@ -3,3 +3,32 @@ About =
   i: ->
 
     _.off '.preloader'
+
+    About.handlers()
+
+
+  handlers: ->
+
+    $('.capcenter > .capmenu > .cap').on 'click', About.capcenter
+    $('.fived > .fivedmenu > .item').on 'click', About.fived
+
+    $('.about > .filters > .inner > .filtermenu > .filter').on 'click', About.menu
+
+  menu: ->
+    section = $(this).html().trim()
+
+    _.off '.about > .filters > .inner > .filtermenu > .filter', '.sections > .section'
+    _.on this, ".sections > .section.section_#{section}"
+
+  capcenter: ->
+
+    cap = $(this).html().trim()
+    _.off '.capcenter > .capmenu > .cap', '.capcenter > .descs > .desc'
+    _.on this, ".capcenter > .descs > .desc_#{cap}"
+
+  fived: ->
+
+    item = $(this).find('.copy > .name').html().trim()
+    _.off '.fived > .fivedmenu > .item', '.fived > .bodys > .body'
+    _.on this, ".fived > .bodys > .body_#{item}"
+
--- pub/jst/about.js
+++ pub/jst/about.js
@@ -2,6 +2,30 @@ var About;
 
 About = {
   i: function() {
-    return _.off('.preloader');
+    _.off('.preloader');
+    return About.handlers();
+  },
+  handlers: function() {
+    $('.capcenter > .capmenu > .cap').on('click', About.capcenter);
+    $('.fived > .fivedmenu > .item').on('click', About.fived);
+    return $('.about > .filters > .inner > .filtermenu > .filter').on('click', About.menu);
+  },
+  menu: function() {
+    var section;
+    section = $(this).html().trim();
+    _.off('.about > .filters > .inner > .filtermenu > .filter', '.sections > .section');
+    return _.on(this, ".sections > .section.section_" + section);
+  },
+  capcenter: function() {
+    var cap;
+    cap = $(this).html().trim();
+    _.off('.capcenter > .capmenu > .cap', '.capcenter > .descs > .desc');
+    return _.on(this, ".capcenter > .descs > .desc_" + cap);
+  },
+  fived: function() {
+    var item;
+    item = $(this).find('.copy > .name').html().trim();
+    _.off('.fived > .fivedmenu > .item', '.fived > .bodys > .body');
+    return _.on(this, ".fived > .bodys > .body_" + item);
   }
 };

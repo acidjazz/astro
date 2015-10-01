42e9dcb316b2c63825982e4c65b6b170ec5143c8
kevin olson <acidjazz@gmail.com>
2015-09-30 17:30:53 -0700

    eightarms

--- scr/thumbs.php
+++ scr/thumbs.php
@@ -7,14 +7,16 @@ if ($handle = opendir($dir)) {
   while (false !== ($entry  = readdir($handle))) {
 
     if ($entry != '.' && $entry != '..' && $entry != '.DS_Store') {
-      echo "mkdir $dir$entry/1440\n";
-      exec("mkdir $dir$entry/1440");
-      echo "cp $dir$entry/* $dir$entry/1440/.\n";
-      exec("cp $dir$entry/* $dir$entry/1440/.");
-      echo "cd $dir$entry/1440\n";
-      chdir("$dir$entry/1440");
-      echo "mogrify -geometry 1440x *\n";
-      exec("mogrify -geometry 1440x *");
+      if (!is_dir($dir.$entry.'/1440')) {
+        echo "mkdir $dir$entry/1440\n";
+        exec("mkdir $dir$entry/1440");
+        echo "cp $dir$entry/* $dir$entry/1440/.\n";
+        exec("cp $dir$entry/* $dir$entry/1440/.");
+        echo "cd $dir$entry/1440\n";
+        chdir("$dir$entry/1440");
+        echo "mogrify -geometry 1440x *\n";
+        exec("mogrify -geometry 1440x *");
+      }
     }
 
   }

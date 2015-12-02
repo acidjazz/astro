052773a8f0d19064e3b28d7ed0624893c0e466e4
kevin olson <acidjazz@gmail.com>
2015-12-02 14:47:01 -0800

    we be gulpin

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+
+var gulp = require('gulp');
+
+var sync = require('browser-sync').create();
+
+var notify = require('gulp-notify');
+
+var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+var stylus = require('gulp-stylus');
+var jade = require('gulp-jade');
+
+var sourcemaps = require('gulp-sourcemaps');
+
+var objectus = require('objectus');
+
+var data = objectus('dat/');
+
+gulp.task('objectus', function() {
+  data = objectus('dat/');
+  return true;
+});
+
+gulp.task('vendors', function() {
+
+  gulp.src([
+      'bower_components/jquery/dist/jquery.js'
+  ])
+  .pipe(sourcemaps.init())
+  .pipe(uglify())
+  .pipe(concat('vendor.min.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('pub/jst/lib'))
+
+});
+
+gulp.task('coffee', function() {
+  gulp.src('cof/**/*.coffee')
+    .pipe(sourcemaps.init())
+    .pipe(coffee({bare: true})
+      .on('error', notify.onError(function(error) {
+        return "Coffee error: " + error.message;
+      }))
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('pub/jst'))
+    .pipe(sync.stream());
+});
+
+gulp.task('stylus', function() {
+  gulp.src('sty/main.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus({ rawDefine: { data: data } })
+    .on('error', notify.onError(function(error) {
+      return "Stylus error: " + error.message;
+    }))
+    .on('error', function(error) {
+      console.log(error);
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('pub/css'))
+    .pipe(sync.stream());
+});
+
+gulp.task('jade', function() {
+  gulp.src('tpl/**/index.jade')
+    .pipe(jade({pretty: true, locals: {data: data}})
+      .on('error', notify.onError(function(error) {
+        return "JADE error: " + error.message;
+      }))
+      .on('error', function(error) {
+        console.log(error);
+      })
+    )
+    .pipe(gulp.dest('pub'))
+    .pipe(sync.stream());
+});
+
+gulp.task('sync', function() {
+  sync.init({
+    notify: false,
+    open: false,
+    server: {
+      baseDir: 'pub/',
+      }
+    });
+
+  gulp.watch('dat/**/*', ['objectus', 'coffee','stylus','jade']);
+  gulp.watch('cof/**/*.coffee', ['coffee']);
+  gulp.watch('sty/**/*.styl', ['stylus']);
+  gulp.watch('tpl/**/*.jade', ['jade']);
+
+});
+
+gulp.task('watch', function() {
+  gulp.watch('dat/**/*', ['objectus', 'coffee','stylus','jade']);
+  gulp.watch('cof/**/*.coffee', ['coffee']);
+  gulp.watch('sty/**/*.styl', ['stylus']);
+  gulp.watch('tpl/**/*.jade', ['jade']);
+});
+
+gulp.task('default', ['objectus', 'coffee', 'stylus', 'jade', 'vendors']);
+
+
--- pub/jst/guide.js
+++ pub/jst/guide.js
@@ -5,3 +5,5 @@ Guide = {
     return _.off('.orbit');
   }
 };
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImd1aWRlLmNvZmZlZSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxJQUFBOztBQUFBLEtBQUEsR0FFRTtFQUFBLENBQUEsRUFBRyxTQUFBO1dBRUQsQ0FBQyxDQUFDLEdBQUYsQ0FBTSxRQUFOO0VBRkMsQ0FBSCIsImZpbGUiOiJndWlkZS5qcyIsInNvdXJjZVJvb3QiOiIvc291cmNlLyIsInNvdXJjZXNDb250ZW50IjpbIkd1aWRlID1cblxuICBpOiAtPlxuXG4gICAgXy5vZmYgJy5vcmJpdCdcbiJdfQ==
